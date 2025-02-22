105e23b630cb5afdd7a6bcdb659582fbf2aa36da
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-02-22 14:47:25 +0100

    Add: Entity Framework Core context and Note model

---
+++ Disparonote/Context/DisparoNoteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+public class DisparoNoteDbContext : IdentityDbContext<IdentityUser>
+{
+    public DisparoNoteDbContext(DbContextOptions<DisparoNoteDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Note> Notes { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Note>()
+            .HasIndex(n => n.AccessKey)
+            .IsUnique();
+    }
+}
---
+++ Disparonote/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Note
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+
+    [Required]
+    [MaxLength(128)]
+    public string AccessKey { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? ExpiresAt { get; set; }
+
+    public bool IsRead { get; set; } = false;
+
+
+}
