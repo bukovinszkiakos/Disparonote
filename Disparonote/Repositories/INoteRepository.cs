094521fd6e3ca0d67e45da83df5a7d103f4ab7c6
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-02-22 14:48:59 +0100

    Add: repository pattern for Note entity

---
+++ Disparonote/Repositories/INoteRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface INoteRepository
+{
+    Task<Note> CreateNoteAsync(Note note);
+    Task<Note?> GetNoteByAccessKeyAsync(string accessKey);
+    Task DeleteNoteAsync(Note note);
+}
---
+++ Disparonote/Repositories/NoteRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class NoteRepository : INoteRepository
+{
+    private readonly DisparoNoteDbContext _context;
+
+    public NoteRepository(DisparoNoteDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Note> CreateNoteAsync(Note note)
+    {
+        _context.Notes.Add(note);
+        await _context.SaveChangesAsync();
+        return note;
+    }
+    public async Task<Note?> GetNoteByAccessKeyAsync(string accessKey)
+    {
+        return await _context.Notes.FirstOrDefaultAsync(n => n.AccessKey == accessKey);
+    }
+    public async Task DeleteNoteAsync(Note note)
+    {
+        _context.Notes.Remove(note);
+        await _context.SaveChangesAsync();
+    }
+
+}
