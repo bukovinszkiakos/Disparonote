7efbf360cc4f4c66e8d0e46bd52fe284722a09e8
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-02-24 20:07:10 +0100

    refactor(footer): improve styling and structure

--- Disparonote Frontend/my-next-app/app/components/Footer/Footer.jsx
+++ Disparonote Frontend/my-next-app/app/components/Footer/Footer.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from "react";
 import Link from "next/link";
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-        <span></span>
-        <li>
-            <ol><Link href={"/about"} className='footer-link'>About</Link></ol>
-            <ol><Link href={"/privacy"} className='footer-link'>Privacy Policy</Link></ol>
-            <ol><Link href={"/tnc"} className='footer-link'>Terms and Conditions</Link></ol>
-            <ol><Link href={"/learn-more"} className='footer-link'>Other Information</Link></ol>
-        </li>
+    <footer className="footer">
+      <ul>
+        <li><Link href="/about" className="footer-link">About</Link></li>
+        <li><Link href="/privacy" className="footer-link">Privacy Policy</Link></li>
+        <li><Link href="/tnc" className="footer-link">Terms & Conditions</Link></li>
+        <li><Link href="/learn-more" className="footer-link">Other Information</Link></li>
+      </ul>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
