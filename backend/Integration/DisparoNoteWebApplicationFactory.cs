54e00af52b38f75d2e4b339146d796f020afe7a1
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-03-31 15:14:36 +0200

    test: add WebApplicationFactory setup for integration testing

---
+++ backend/Integration/DisparoNoteWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+public class DisparoNoteWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly string _dbName = Guid.NewGuid().ToString();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("IntegrationTest");
+
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d =>
+                d.ServiceType == typeof(DbContextOptions<DisparoNoteDbContext>));
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<DisparoNoteDbContext>(options =>
+                options.UseInMemoryDatabase(_dbName));
+
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<DisparoNoteDbContext>();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+        });
+    }
+}
